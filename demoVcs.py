5ca784c124df6822b4a50e97d65e976ea3904a51
Nulifyy <yuheng2011@hotmail.com>
2021-07-21 10:45:22 +0800

    v1.0 (add and minus)

---
+++ demoVcs.py
@@ -0,0 +1,9 @@
+class calculator:
+    def addNum(num1,num2):
+        sumAdd = num1 + num2;
+        print(sumAdd)
+    def minusNum(num1,num2):
+        sumSub = num1 - num2;
+        print(sumSub)
+calculator.addNum(50,40)
+calculator.minusNum(50,40)
